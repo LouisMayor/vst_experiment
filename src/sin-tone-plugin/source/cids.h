6611c2a9eca21e455f58fa4637a559bb78d6a86a
Louis Mayor <louismayor1@gmail.com>
2026-04-10 23:57:50 +0100

    added todo comments to sin-tone cids.h

--- src/sin-tone-plugin/source/cids.h
+++ src/sin-tone-plugin/source/cids.h
@@ -3,10 +3,15 @@
 #include "pluginterfaces/base/funknown.h"
 #include "pluginterfaces/vst/vsttypes.h"
 
+// todo: take out of Tutorial namespace (gh_issue: https://github.com/LouisMayor/vst_experiment/issues/1)
 namespace Steinberg::Tutorial {
+
+// todo: write uuid generator and update uuid (gh_issue: https://github.com/LouisMayor/vst_experiment/issues/2)
+// uuidgen MacOS/Linux
+// [guid]::NewGuid() Windows Powershell
 static const FUID processor_uid (0xC18D3C1E, 0x719E4E29, 0x924D3ECA, 0xA5E4DA18);
 static const FUID controller_uid (0xC244B7E6, 0x24084E20, 0x24A8C43, 0xF84C8BE8);
 
-#define sin_tone_vst3_category "Fx"
+#define sin_tone_vst3_category "Instrument"
 
 }
