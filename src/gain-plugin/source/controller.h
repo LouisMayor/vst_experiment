14a93ad94bd04170711c302453cdbd134c338ca3
Louis Mayor <louismayor1@gmail.com>
2026-04-26 18:05:18 +0100

    applied formating to gain-plugin src files

--- src/gain-plugin/source/controller.h
+++ src/gain-plugin/source/controller.h
@@ -11,38 +11,36 @@ namespace GainVendor {
 //------------------------------------------------------------------------
 //  GainController
 //------------------------------------------------------------------------
-class GainController : public Steinberg::Vst::EditControllerEx1
-{
-public:
-//------------------------------------------------------------------------
-	GainController () = default;
-	~GainController () SMTG_OVERRIDE = default;
+class GainController : public Steinberg::Vst::EditControllerEx1 {
+  public:
+	//------------------------------------------------------------------------
+	GainController()				= default;
+	~GainController() SMTG_OVERRIDE = default;
 
-    // Create function
-	static Steinberg::FUnknown* createInstance (void* /*context*/)
-	{
-		return (Steinberg::Vst::IEditController*)new GainController;
+	// Create function
+	static Steinberg::FUnknown *createInstance(void * /*context*/) {
+		return (Steinberg::Vst::IEditController *)new GainController;
 	}
 
 	//--- from IPluginBase -----------------------------------------------
-	Steinberg::tresult PLUGIN_API initialize (Steinberg::FUnknown* context) SMTG_OVERRIDE;
-	Steinberg::tresult PLUGIN_API terminate () SMTG_OVERRIDE;
+	Steinberg::tresult PLUGIN_API initialize(Steinberg::FUnknown *context) SMTG_OVERRIDE;
+	Steinberg::tresult PLUGIN_API terminate() SMTG_OVERRIDE;
 
 	//--- from EditController --------------------------------------------
-	Steinberg::tresult PLUGIN_API setComponentState (Steinberg::IBStream* state) SMTG_OVERRIDE;
-	Steinberg::IPlugView* PLUGIN_API createView (Steinberg::FIDString name) SMTG_OVERRIDE;
-	Steinberg::tresult PLUGIN_API setState (Steinberg::IBStream* state) SMTG_OVERRIDE;
-	Steinberg::tresult PLUGIN_API getState (Steinberg::IBStream* state) SMTG_OVERRIDE;
+	Steinberg::tresult PLUGIN_API	 setComponentState(Steinberg::IBStream *state) SMTG_OVERRIDE;
+	Steinberg::IPlugView *PLUGIN_API createView(Steinberg::FIDString name) SMTG_OVERRIDE;
+	Steinberg::tresult PLUGIN_API	 setState(Steinberg::IBStream *state) SMTG_OVERRIDE;
+	Steinberg::tresult PLUGIN_API	 getState(Steinberg::IBStream *state) SMTG_OVERRIDE;
 
- 	//---Interface---------
+	//---Interface---------
 	DEFINE_INTERFACES
-		// Here you can add more supported VST3 interfaces
-		// DEF_INTERFACE (Vst::IXXX)
-	END_DEFINE_INTERFACES (EditController)
-    DELEGATE_REFCOUNT (EditController)
+	// Here you can add more supported VST3 interfaces
+	// DEF_INTERFACE (Vst::IXXX)
+	END_DEFINE_INTERFACES(EditController)
+	DELEGATE_REFCOUNT(EditController)
 
-//------------------------------------------------------------------------
-protected:
+	//------------------------------------------------------------------------
+  protected:
 };
 
 //------------------------------------------------------------------------
--- src/gain-plugin/source/entry.cpp
+++ src/gain-plugin/source/entry.cpp
@@ -2,9 +2,9 @@
 // Copyright(c) 2026 My Plug-in Company.
 //------------------------------------------------------------------------
 
-#include "processor.h"
-#include "controller.h"
 #include "cids.h"
+#include "controller.h"
+#include "processor.h"
 #include "version.h"
 
 #include "public.sdk/source/main/pluginfactory.h"
@@ -18,33 +18,31 @@ using namespace GainVendor;
 //  VST Plug-in Entry
 //------------------------------------------------------------------------
 
-BEGIN_FACTORY_DEF ("My Plug-in Company", 
-			       "https://www.mycompanyname.com", 
-			       "mailto:info@mycompanyname.com")
+BEGIN_FACTORY_DEF("My Plug-in Company", "https://www.mycompanyname.com", "mailto:info@mycompanyname.com")
 
-	//---First Plug-in included in this factory-------
-	// its kVstAudioEffectClass component
-	DEF_CLASS2 (INLINE_UID_FROM_FUID(kGainProcessorUID),
-				PClassInfo::kManyInstances,	// cardinality
-				kVstAudioEffectClass,	// the component category (do not changed this)
-				stringPluginName,		// here the Plug-in name (to be changed)
-				Vst::kDistributable,	// means that component and controller could be distributed on different computers
-				GainVST3Category, // Subcategory for this Plug-in (to be changed)
-				FULL_VERSION_STR,		// Plug-in version (to be changed)
-				kVstVersionString,		// the VST 3 SDK version (do not changed this, use always this define)
-				GainProcessor::createInstance)	// function pointer called when this component should be instantiated
+//---First Plug-in included in this factory-------
+// its kVstAudioEffectClass component
+DEF_CLASS2(INLINE_UID_FROM_FUID(kGainProcessorUID),
+		   PClassInfo::kManyInstances,	  // cardinality
+		   kVstAudioEffectClass,		  // the component category (do not changed this)
+		   stringPluginName,			  // here the Plug-in name (to be changed)
+		   Vst::kDistributable,			  // means that component and controller could be distributed on different computers
+		   GainVST3Category,			  // Subcategory for this Plug-in (to be changed)
+		   FULL_VERSION_STR,			  // Plug-in version (to be changed)
+		   kVstVersionString,			  // the VST 3 SDK version (do not changed this, use always this define)
+		   GainProcessor::createInstance) // function pointer called when this component should be instantiated
 
-	// its kVstComponentControllerClass component
-	DEF_CLASS2 (INLINE_UID_FROM_FUID (kGainControllerUID),
-				PClassInfo::kManyInstances, // cardinality
-				kVstComponentControllerClass,// the Controller category (do not changed this)
-				stringPluginName "Controller",	// controller name (could be the same than component name)
-				0,						// not used here
-				"",						// not used here
-				FULL_VERSION_STR,		// Plug-in version (to be changed)
-				kVstVersionString,		// the VST 3 SDK version (do not changed this, use always this define)
-				GainController::createInstance)// function pointer called when this component should be instantiated
+// its kVstComponentControllerClass component
+DEF_CLASS2(INLINE_UID_FROM_FUID(kGainControllerUID),
+		   PClassInfo::kManyInstances,	   // cardinality
+		   kVstComponentControllerClass,   // the Controller category (do not changed this)
+		   stringPluginName "Controller",  // controller name (could be the same than component name)
+		   0,							   // not used here
+		   "",							   // not used here
+		   FULL_VERSION_STR,			   // Plug-in version (to be changed)
+		   kVstVersionString,			   // the VST 3 SDK version (do not changed this, use always this define)
+		   GainController::createInstance) // function pointer called when this component should be instantiated
 
-	//----for others Plug-ins contained in this factory, put like for the first Plug-in different DEF_CLASS2---
+//----for others Plug-ins contained in this factory, put like for the first Plug-in different DEF_CLASS2---
 
 END_FACTORY
